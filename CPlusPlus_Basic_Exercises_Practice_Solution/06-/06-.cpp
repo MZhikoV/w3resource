08ec23d4af8b84d25b04e266579bc913d08852f0
MZhikoV <m.zhikov@gmail.com>
2023-12-12 16:44:44 +0200

    more solutions

---
+++ CPlusPlus_Basic_Exercises_Practice_Solution/06-/06-.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    char gender = 'F';
+    bool married = 1;
+    int sons = 2;
+    int appYear = 2009;
+    int salaryY = 1500000;
+    float height = 79.48;
+    float   GPA = 4.69;
+    int salaryDrawn = 12047235;
+    double balance = 995324987;
+
+
+
+    cout<<"The Gender is :"<< F
+        cout << "Is she married ? :" 1
+        cout << "Number of sons she has :" 2
+        cout << "Year of her appointment :" 2009
+        cout << "Salary for a year :" 1500000
+        cout << "Height is : "79.48
+        cout << "GPA is "4.69
+        cout << "Salary drawn upto :" 12047235
+        cout << "Balance till :" 995324987
+
+}
