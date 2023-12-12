7ace4c0a87c62d224cda9932e434be94d6b605e4
MZhikoV <m.zhikov@gmail.com>
2023-12-12 16:06:02 +0200

    some solutions

---
+++ CPlusPlus_Basic_Exercises_Practice_Solution/01.Welcome/01.Welcome.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+
+    cout << 29 + 30 << endl;
+
+
+    cout << "The size of some fundamental types are:" << endl;
+    cout << "char " << sizeof(char) << endl;
+    cout << "short " << sizeof(short) << endl;
+    cout << "int " << sizeof(int) << endl;
+    cout << "long "<<sizeof(long) << endl;
+    cout << "lomg long "<<sizeof(long long) << endl;
+    cout << "float "<<sizeof(float) << endl;
+    cout << "double "<<sizeof(double) << endl;
+    cout << "long double "<<sizeof(long double) << endl;
+    cout << "bool "<<sizeof(bool) << endl;
+
+    int a, b;
+    a = 29;
+    b = 30;
+
+    int sum = a + b;
+
+    cout << sum << endl;
+
+    cout << endl;
+
+    cout << "The maximum limit of int data type :" << INT_MAX << endl;
+    cout << "The minimum limit of int data type : " << INT_MIN << endl;
+        cout << "The maximum limit of unsigned int data type : "<< UINT_MAX << endl;
+        cout << "The maximum limit of long long data type : "<< LLONG_MAX << endl;
+        cout << "The minimum limit of long long data type : "<< LLONG_MIN << endl;
+        cout << "The maximum limit of unsigned long long data type : "<< ULLONG_MAX<< endl;
+        cout << "The Bits contain in char data type : "<< CHAR_BIT<< endl;
+        cout << "The maximum limit of char data type : "<< CHAR_MAX<< endl;
+        cout << "The minimum limit of char data type : "<< CHAR_MIN<< endl;
+        cout << "The maximum limit of signed char data type : "<<SCHAR_MAX<< endl;
+        cout << "The minimum limit of signed char data type : "<< SCHAR_MIN<< endl;
+        cout << "The maximum limit of unsigned char data type : "<< UCHAR_MAX<< endl;
+        cout << "The minimum limit of short data type : "<< SHRT_MIN << endl;
+        cout << "The maximum limit of short data type : "<< SHRT_MAX<< endl;
+        cout << "The maximum limit of unsigned short data type : "<< USHRT_MAX << endl;
+
+        cout << endl;
+
+
+    return 0;
+}
