46b71b364536cf1a3e191e696a062dc04ab58b54
MZhikoV <m.zhikov@gmail.com>
2024-02-22 15:40:37 +0200

    Task 05 solution

---
+++ CPlusPlus_File_handling/Task05.cpp
@@ -0,0 +1,42 @@
+//5. Write a C++ program to copy the contents of one text file to another.
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <memory>
+#include <fstream>
+
+
+using namespace std;
+
+
+
+int main() {
+
+    ifstream inpF("Task03_test.txt");
+    ofstream outF("Task05_result.txt");
+
+    if (inpF.is_open()) {
+        string line;
+        while (getline(inpF,line)) {
+            outF<<line<<endl;
+        }
+
+        inpF.close();
+        outF.close();
+    }
+    else {
+        cout<<"Error reading from file!"<<endl;
+    }
+
+    cout<<endl;
+    
+    
+    system("pause");
+    return 0;
+}
