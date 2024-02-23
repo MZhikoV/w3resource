0913a0ca993c2eca9b5c54af4513b6c3f35e01ef
MZhikoV <m.zhikov@gmail.com>
2024-02-23 09:57:47 +0200

    update

---
+++ 99.Template.cpp
@@ -0,0 +1,22 @@
+//
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <memory>
+
+using namespace std;
+
+
+
+int main() {
+    
+    
+    system("pause");
+    return 0;
+}
